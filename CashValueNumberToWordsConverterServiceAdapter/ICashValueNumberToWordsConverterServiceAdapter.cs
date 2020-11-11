d0997f4415af9e571523b858d872d1e7c74ec4a5
Ingmar Scheffler <ingmarscheffler@hotmail.de>
2020-11-11 14:43:50 +0100

    extra adapter project added

---
+++ CashValueNumberToWordsConverterServiceAdapter/ICashValueNumberToWordsConverterServiceAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CashValueNumberToWordsConverterServiceAdapter
+{
+    public interface ICashValueNumberToWordsConverterServiceAdapter
+    {
+        Task<ConversionResult> Convert(double number);
+    }
+}
